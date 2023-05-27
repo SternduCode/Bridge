9e81704fdc09948024d6cd14dc223b44814f4b79
Sterndu <Sterndu13570@gmail.com>
2023-05-28 01:57:23 +0200

    fixes

--- src/com/sterndu/bridge/BridgeCLI.java
+++ src/com/sterndu/bridge/BridgeCLI.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 import com.sterndu.data.transfer.Connector;
 import com.sterndu.multicore.Updater;
-import com.sterndu.util.kotlin.Entry;
+import com.sterndu.util.Entry;
 
 
 // TODO: Auto-generated Javadoc
--- src/com/sterndu/bridge/BridgeServer.java
+++ src/com/sterndu/bridge/BridgeServer.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 import com.sterndu.data.transfer.secure.*;
 import com.sterndu.multicore.Updater;
-import com.sterndu.util.kotlin.Entry;
+import com.sterndu.util.Entry;
 
 // TODO: Auto-generated Javadoc
 /**
